727abb75906842b744141caa62fbfeb4a6486e59
MStudent1 <mlancaster17@ivytech.edu>
2022-04-28 17:36:15 -0400

    Added delete event handler to remove the document object specified by the id; event occurs once the details.ejs delete button is activated

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog'); //Inserts blog.js which contains the schema and model
-const { render } = require('ejs');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
-//Connect to mongodb
-const dbURI = 'mongodb+srv://new-user-1:ZwTyy5lamaIEsQUf@cluster0.uk7mx.mongodb.net/node-tuts?retryWrites=true&w=majority';
-mongoose.connect(dbURI,  { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
 
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://new-user-1:ZwTyy5lamaIEsQUf@cluster0.uk7mx.mongodb.net/node-tuts?retryWrites=true&w=majority";
 
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true })); //Middleware used for accepting form data
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
+// routes
 app.get('/', (req, res) => {
   res.redirect('/blogs');
 });
@@ -35,47 +34,55 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
-//Passes the document objects of the collection blogs to the index html page
-//The html page contains
-//Blog routes
+// blog routes
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+
 app.get('/blogs', (req, res) => {
-  Blog.find().sort({ createdAt: -1 }) //Displays the blogs in descending order based on the time stamp the blogs were created
-  .then((result) => {
-    res.render('index', { title: 'All Blogs', blogs: result })
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-})
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', { blogs: result, title: 'All blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-//POST event handler that adds a new blog to the index html page
-app.post(('/blogs'), (req, res) => {
-  const blog = new Blog(res.body)
-  
+app.post('/blogs', (req, res) => {
+  // console.log(req.body);
+  const blog = new Blog(req.body);
+
   blog.save()
-  .then((result) => {
-    res.redirect('/blogs');
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-})
+    .then(result => {
+      res.redirect('/blogs');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-//Uses id to access blog via an id
 app.get('/blogs/:id', (req, res) => {
   const id = req.params.id;
-
   Blog.findById(id)
     .then(result => {
       res.render('details', { blog: result, title: 'Blog Details' });
     })
     .catch(err => {
       console.log(err);
-    })
-})
+    });
+});
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 // 404 page
