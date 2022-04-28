2e65cc8d14380b4ad1f6251622db09f64eae59c1
MStudent1 <mlancaster17@ivytech.edu>
2022-04-28 14:16:55 -0400

    Added new file to folder models that added the schema and model for the blog collection

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
+//Connect to mongodb
+const dbURI = 'mongodb+srv://new-user-1:ZwTyy5lamaIEsQUf@cluster0.uk7mx.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI,  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
-// listen for requests
-app.listen(3000);
+
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -13,21 +18,12 @@ app.set('view engine', 'ejs');
 // middleware & static files
 app.use(express.static('public'));
 
-//Middleware
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();
-});
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
   console.log('in the next middleware');
   next();
 });
-
-app.use(morgan('dev'));
 
 app.use((req, res, next) => {
   res.locals.path = req.path;
